1169d20b945022052f95c920f396836c1ec5967a
Kyle Mullins <kylebmullins@gmail.com>
2020-09-04 11:53:01 -0600

    Complete DoubleJeopardy

---
+++ Chapter8/DoubleJeopardy.java
@@ -0,0 +1,8 @@
+public class DoubleJeopardy
+{
+  public static void main ( String[] args )
+  {
+    double value = 1.7E+309;
+    System.out.println("A double: " +  value);
+  }
+}
--- Chapter8/Shortfall.java
+++ Chapter8/Shortfall.java
@@ -1,8 +1,9 @@
-public class Shortfall
-{
-  public static void main ( String[] args )
+public class Shortfall {
+
+    public static void main ( String[] args )
   {
     int value = 35000;
     System.out.println("A short: " +  value);
   }
+
 }
