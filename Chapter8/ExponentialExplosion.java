6f37a30b06996124b63f6cb1ea5b0c9d342dff48
Kyle Mullins <kylebmullins@gmail.com>
2020-09-08 10:42:51 -0600

    Finish Exercise 3

---
+++ Chapter8/ExponentialExplosion.java
@@ -0,0 +1,8 @@
+public class ExponentialExplosion
+{
+  public static void main ( String[] args )
+  {
+    double value = 710;
+    System.out.println("e to the power value: " +  Math.exp( value ) );
+  }
+}
