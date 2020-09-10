02bc13e116b0c071cdf5b5f587b8cfea410d87a6
Kyle Mullins <kylebmullins@gmail.com>
2020-09-10 11:22:45 -0600

    Complete exercise 2

---
+++ Chapter10/Trig.java
@@ -0,0 +1,9 @@
+public class Trig {
+    public static void main(String[] args) {
+        double w = 1;
+        double x = Math.sin(w);
+        double y = Math.cos(w);
+        double z = (y * y) + (x * x);
+        System.out.println(x+ ", " + y + ", " + z);
+    }
+}
