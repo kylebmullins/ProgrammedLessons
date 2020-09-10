df4da88b251fd14b490bcb0aac881c1f5edff59f
Kyle Mullins <kylebmullins@gmail.com>
2020-09-10 11:05:58 -0600

    Finish exercises 2 and 3

---
+++ Chapter9/Quadratic.java
@@ -0,0 +1,14 @@
+public class Quadratic {
+     public static void main(String[] args) {
+        double x = 0.0;
+        double value = ((3 * x * x) - (8 * x) + 4);
+        System.out.println(value);
+        x = 2.0;
+        value = ((3 * x * x) - (8 * x) + 4);
+        System.out.println(value);
+        x = 4.0;
+        value = ((3 * x * x) - (8 * x) + 4);
+        System.out.println(value);
+    }
+
+}
