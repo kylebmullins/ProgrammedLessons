dbb3faf97890b8bff5f4864ce8a78e645b957192
Kyle Mullins <kylebmullins@gmail.com>
2020-09-10 11:14:46 -0600

    Finish exercise 1

---
+++ Chapter10/Rain.java
@@ -0,0 +1,12 @@
+public class Rain {
+    public static void main(String[] args) {
+        double a = 12;
+        double m = 14;
+        double j = 8;
+        double av = (a + m + j) / 3;
+        System.out.println("Rainfall for April: " + a);
+        System.out.println("Rainfall for May: " + m);
+        System.out.println("Rainfall for June: " + j);
+        System.out.println("Average rainfall: " + av);
+    }
+}
