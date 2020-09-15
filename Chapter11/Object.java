6f8b31a002468db3935eaf3d03d3501e0fe05db8
Kyle Mullins <kylebmullins@gmail.com>
2020-09-15 11:16:10 -0600

    Complete excercise 1

---
+++ Chapter11/Object.java
@@ -0,0 +1,8 @@
+public class Object {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str);
+        System.out.println(secondVariable);
+        
+    }
+}
