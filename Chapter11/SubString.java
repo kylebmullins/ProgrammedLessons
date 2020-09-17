0c8151c346b0b0bb56d9c9c54e9b1abc47ab8580
Kyle Mullins <kylebmullins@gmail.com>
2020-09-16 19:40:59 -0600

    Complete Exercise 4

---
+++ Chapter11/SubString.java
@@ -0,0 +1,7 @@
+public class SubString {
+    public static void main(String[] args) {
+        String str = new String( "Golf is a good walk spoiled." );
+        String sub = str.substring(10, 19);
+        System.out.print(sub);
+    }
+}
