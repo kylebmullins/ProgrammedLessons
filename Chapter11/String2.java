06c6571b2282c1430eb659a77cac5d588130c724
Kyle Mullins <kylebmullins@gmail.com>
2020-09-16 19:35:26 -0600

    Complete exercise 3

--- Chapter11/String2.java
+++ Chapter11/String2.java
@@ -1,8 +1,8 @@
 public class String2 {
     public static void main(String[] args) {
         String first = new String("    In a Hole    in the ground there lived a Hobbit.   ");
-    String second;
-    second = first.trim();
-    System.out.println(first+" 1 \n"+second+" 1");
+        String second;
+        second = first.trim();
+        System.out.println(first+" 1 \n"+second+" 1");
     }
 }
