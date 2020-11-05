6d3988869f902604fa656c03e9ed8e2e53828a35
Kyle Mullins <kylebmullins@gmail.com>
2020-11-05 10:39:50 -0700

    Simplify parts of code being used several different times

--- Color/Color.java
+++ Color/Color.java
@@ -5,33 +5,17 @@ public class Color {
     int r = 0;
     int g = 0;
     int b = 0;
-    double rMath;
-    double gMath;
-    double bMath;
     public Color(int red, int grn, int blu) {
-        if (red > 255) {
-            red = 255;
-        } else if (red < 0) {
-            red = 0;
-        } else if (grn > 255) {
-            grn = 255;
-        } else if (grn < 0) {
-            grn = 0;
-        } else if (blu > 255) {
-            blu = 255;
-        } else if (red < 0) {
-            blu = 0;
-        }
-        r = red;
-        g = grn;
-        b = blu;
+        r = fitDomain(red);
+        g = fitDomain(grn);
+        b = fitDomain(blu);
     }
 
     public Color() {
         randomize();
     }
 
-    public void summary() {
+    public void getColor() {
         System.out.println("[" + r + ", " + g + ", " + b + "]");
     }
 
@@ -44,72 +28,56 @@ public class Color {
     public void lighten(double percent) {
         if (percent >= 0) {
             double p = (percent + 100);
-            rMath = (double)r*p/100.0;
-            gMath = (double)g*p/100.0;
-            bMath = (double)b*p/100.0;
-            r = (int)rMath;
-            g = (int)gMath;
-            b = (int)bMath;
-            if (r > 255) {
-                r = 255;
-            }
-            if (g > 255) {
-                g = 255;
-            }
-            if (b > 255) {
-                b = 255;
-            }
-        } else {
-            System.out.println("Error: Percent value is not positive. Please fix and try again.");
+            r = (int)((double)r*p/100.0);
+            g = (int)((double)g*p/100.0);
+            b = (int)((double)b*p/100.0);
+            fitDomain();
         }
     }
 
     public void darken(double percent) {
         if (percent >= 0 && percent <= 100) {
-            rMath = r - ((double)r*percent/100.0);
-            gMath = g - ((double)g*percent/100.0);
-            bMath = b - ((double)b*percent/100.0);
-            r = (int)rMath;
-            g = (int)gMath;
-            b = (int)bMath;
-            if (r < 0) {
-                r = 0;
-            }
-            if (g < 0) {
-                g = 0;
-            }
-            if (b < 0) {
-                b = 0;
-            }
-        } else {
-            System.out.println("Error: Percent value does not fit in the range 0-100. Please fix and try again.");
+            r = (int)(r - ((double)r*percent/100.0));
+            g = (int)(g - ((double)g*percent/100.0));
+            b = (int)(b - ((double)b*percent/100.0));
+            fitDomain();
         }
     }
 
-    public void r(int x) {
-        if (x > 255) {
-            x = 255;
-        } if (x < 0) {
-            x = 0;
-        }
-        r = x;
+    public void setRed(int x) {
+        r = fitDomain(x);
     }
 
-    public void g(int x) {
-        if (x > 255) {
-            x = 255;
-        } if (x < 0) {
-            x = 0;
-        }
-        g = x;
+    public void setGreen(int x) {
+        g = fitDomain(x);
     }
 
-    public void b(int x) {
+    public void setBlue(int x) {
+        b = fitDomain(x);
+    }
+
+    public int fitDomain(int x) {
         if (x > 255) {
             x = 255;
-        } if (x < 0) {
+        } else if (x < 0) {
             x = 0;
         }
-        b = x;
+        return x;
+    }
+
+    public void fitDomain() {
+        if (r > 255) {
+            r = 255;
+        } else if (r < 0) {
+            r = 0;
+        } else if (g > 255) {
+            g = 255;
+        } else if (g < 0) {
+            g = 0;
+        } else if (b > 255) {
+            b = 255;
+        } else if (b < 0) {
+            b = 0;
+        }
     }
 }
