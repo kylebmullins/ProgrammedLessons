54ed77aa7f1b1b8a9a63712e97b3989b50b1b778
Kyle Mullins <kylebmullins@gmail.com>
2020-11-05 11:02:11 -0700

    Tested the Color() function without parameters

--- Color/ColorTester.java
+++ Color/ColorTester.java
@@ -2,16 +2,18 @@
 public class ColorTester {
     public static void main(String[] args) {
         Color test = new Color(1, 5, 244);
-        test.summary();
+        Color testTwo = new Color();
+        test.getColor();
         test.lighten(100);
-        test.summary();
+        test.getColor();
         test.darken(50);
-        test.summary();
+        test.getColor();
         test.randomize();
-        test.summary();
-        test.r(10);
-        test.g(20);
-        test.b(30);
-        test.summary();
+        test.getColor();
+        test.setRed(10);
+        test.setGreen(20);
+        test.setBlue(30);
+        test.getColor();
+        testTwo.getColor();
     }
 }
